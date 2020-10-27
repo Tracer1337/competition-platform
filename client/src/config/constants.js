29b670faeea047d3242f596e4f5b77c7e4b41830
Merlin Moelter <joebau.0815@googlemail.com>
2020-10-27 16:27:55 +0100

    Add dynamic discord oauth url generation in frontend

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -1,5 +1,6 @@
 export const API_BASE_URL = window.location.origin + "/api"
-export const DISCORD_OAUTH_URL = `https://discord.com/api/oauth2/authorize?client_id=769821807770861568&redirect_uri${encodeURIComponent(process.env.DISCORD_OAUTH_URL)}=&response_type=code&scope=identify`
+export const DISCORD_OAUTH_API_ENDPOINT = API_BASE_URL + "/auth/discord"
+export const DISCORD_OAUTH_URL = `https://discord.com/api/oauth2/authorize?client_id=769821807770861568&redirect_uri${encodeURIComponent(DISCORD_OAUTH_API_ENDPOINT)}=&response_type=code&scope=identify`
 export const DISCORD_CDN_BASE_URL = "https://cdn.discordapp.com"
 
 export const MAX_UPLOAD_FILE_SIZE = 1024 * 1024 * 5 // 5MB
