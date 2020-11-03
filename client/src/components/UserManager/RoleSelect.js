b7d0589adc74a108f506d6eb8eb8fea8a217f224
Merlin Moelter <joebau.0815@googlemail.com>
2020-11-03 13:56:09 +0100

    Add confirm dialog when changing own role in admin panel

--- client/src/components/UserManager/RoleSelect.js
+++ client/src/components/UserManager/RoleSelect.js
@@ -1,17 +1,31 @@
 import React, { useState } from "react"
+import { useSelector } from "react-redux"
 import { CircularProgress } from "@material-ui/core"
 
 import Select from "../Forms/components/Select.js"
 import useAPIData from "../../utils/useAPIData.js"
 import { editUser } from "../../config/api.js"
+import { opener } from "../ComponentOpener/ComponentOpener.js"
 
 function RoleSelect({ user }) {
+    const me = useSelector(store => store.auth.user)
+    
     const [isSaving, setIsSaving] = useState(false)
     const [roleId, setRoleId] = useState(user.role.id)
 
     const { isLoading, data } = useAPIData("getAllRoles")
 
-    const handleChange = (event) => {
+    const handleChange = async (event) => {
+        if (user.id === me.id) {
+            const dialog = opener.openDialog("Confirm", { content: "Your own role will be changed" })
+
+            const hasAccepted = await new Promise(resolve => dialog.addEventListener("close", resolve))
+
+            if (!hasAccepted) {
+                return
+            }
+        }
+
         setIsSaving(true)
 
         editUser(user.id, {
